491761e1cc22e089dd96dc5ab245ffff0c73e51e
juxin <15155117391@163.com>
2022-10-08 19:51:58 +0800

    1629

---
+++ src/com/LeetCode/solution0941/test/Solution1694_2Test.java
@@ -0,0 +1,58 @@
+package com.LeetCode.solution0941.test;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import com.LeetCode.solution1694.Solution1694_2;
+
+public class Solution1694_2Test {
+
+	@Test
+	public void reformatNumberTest_001() {
+
+		// 预想值
+		String expct = "123-456";
+		// 实际值
+		Solution1694_2 test = new Solution1694_2();
+		String result = test.reformatNumber("1-23-45 6");
+		// 比较结果
+		Assert.assertEquals(expct, result);
+	}
+	
+	@Test
+	public void reformatNumberTest_002() {
+
+		// 预想值
+		String expct = "123-45-67";
+		// 实际值
+		Solution1694_2 test = new Solution1694_2();
+		String result = test.reformatNumber("123 4-567");
+		// 比较结果
+		Assert.assertEquals(expct, result);
+	}
+	
+	@Test
+	public void reformatNumberTest_003() {
+
+		// 预想值
+		String expct = "123-456-78";
+		// 实际值
+		Solution1694_2 test = new Solution1694_2();
+		String result = test.reformatNumber("123 4-5678");
+		// 比较结果
+		Assert.assertEquals(expct, result);
+	}
+	
+	@Test
+	public void reformatNumberTest_004() {
+
+		// 预想值
+		String expct = "175-229-353-94-75";
+		// 实际值
+		Solution1694_2 test = new Solution1694_2();
+		String result = test.reformatNumber("--17-5 229 35-39475 ");
+		// 比较结果
+		Assert.assertEquals(expct, result);
+	}
+
+}
